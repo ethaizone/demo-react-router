7cc63bc2f767ba7ab6c6443d09817a4de0cdef4a
EThaiZone <ethaizone@hotmail.com>
2025-05-10 17:22:00 +0700

    feat: add layout and editorconfig

--- app/routes.ts
+++ app/routes.ts
@@ -1,14 +1,17 @@
 import {
   type RouteConfig,
   index,
+  layout,
   prefix,
   route,
 } from "@react-router/dev/routes";
 
 export default [
-  index("routes/home.tsx"),
-  ...prefix("users", [
-    index("routes/users/index.tsx"),
-    route(":id", "routes/users/item.tsx"),
+  layout("layout.tsx", [
+    index("routes/home.tsx"),
+    ...prefix("users", [
+      index("routes/users/index.tsx"),
+      route(":id", "routes/users/item.tsx"),
+    ]),
   ]),
 ] satisfies RouteConfig;
